4044703baf8dd90716a58093b655badf3f5cf4ee
Henrik Witt-Hansen <hwha@dbc.dk>
2019-10-14 12:39:22 +0200

    Use org.hamcrest for regular asserts
    Auditors: jbn, atm, mib, jbr

--- src/test/java/dk/dbc/weekresolver/ejb/WeekResolverBeanTest.java
+++ src/test/java/dk/dbc/weekresolver/ejb/WeekResolverBeanTest.java
@@ -1,7 +1,11 @@
 package dk.dbc.weekresolver.ejb;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.format.DateTimeParseException;
 
@@ -10,40 +14,40 @@ public class WeekResolverBeanTest {
     @Test
     public void TestInvalidCatalogueCode() {
         WeekResolverBean b = new WeekResolverBean();
-        Assertions.assertThrows(UnsupportedOperationException.class,() -> b.getWeekCode("qxz", "2019-11-29"));
+        assertThrows(UnsupportedOperationException.class,() -> b.getWeekCode("qxz", "2019-11-29"));
     }
 
     @Test
     public void TestInvalidDate() {
         WeekResolverBean b = new WeekResolverBean();
-        Assertions.assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "2019-13-29"));
-        Assertions.assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "2019-11-32"));
-        Assertions.assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "11-29"));
+        assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "2019-13-29"));
+        assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "2019-11-32"));
+        assertThrows(DateTimeParseException.class,() -> b.getWeekCode("bpf", "11-29"));
     }
 
     @Test
     public void TestValidCatalogueCode() {
         WeekResolverBean b = new WeekResolverBean();
-        Assertions.assertDoesNotThrow(() -> b.getWeekCode("bpf", "2019-11-29"));
-        Assertions.assertDoesNotThrow(() -> b.getWeekCode("BPF", "2019-11-29"));
+        assertDoesNotThrow(() -> b.getWeekCode("bpf", "2019-11-29"));
+        assertDoesNotThrow(() -> b.getWeekCode("BPF", "2019-11-29"));
     }
 
     @Test
     public void TestCatalogueCodeBPF() {
         WeekResolverBean b = new WeekResolverBean();
 
-        Assertions.assertDoesNotThrow(() -> b.getWeekCode("bpf", "2019-11-29"));
+        assertDoesNotThrow(() -> b.getWeekCode("bpf", "2019-11-29"));
 
         WeekResolverResult result = b.getWeekCode("bpf", "2019-11-29");
-        Assertions.assertEquals(result.WeekNumber, 48);
-        Assertions.assertEquals(result.Year, 2019);
-        Assertions.assertEquals(result.CatalogueCode, "BPF");
-        Assertions.assertEquals(result.WeekCode, "BPF201948");
+        assertThat(result.WeekNumber, is(48));
+        assertThat(result.Year, is(2019));
+        assertThat(result.CatalogueCode, is("BPF"));
+        assertThat(result.WeekCode, is("BPF201948"));
 
         result = b.getWeekCode("bpf", "2019-12-29");
-        Assertions.assertEquals(result.WeekNumber, 1);
-        Assertions.assertEquals(result.Year, 2020);
-        Assertions.assertEquals(result.CatalogueCode, "BPF");
-        Assertions.assertEquals(result.WeekCode, "BPF202001");
+        assertThat(result.WeekNumber, is(1));
+        assertThat(result.Year, is(2020));
+        assertThat(result.CatalogueCode, is("BPF"));
+        assertThat(result.WeekCode, is("BPF202001"));
     }
 }
