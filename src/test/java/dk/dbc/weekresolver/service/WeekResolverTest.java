28e89772ef9679c68af50fdf0cf94279abb0b47b
Henrik Witt-Hansen <hwha@dbc.dk>
2019-10-17 10:35:02 +0200

    Add test for calculation of the DPF weekcode
    The test is based on unittests in dbckat-config-test/js-use-files/weekcodes.use.js
    The originating test, and the logic is tests, seems to have a few flaws, which have been attempted
    fixed here.
    
    Note: the test does currently fail since the logic is incorrect. Next commit will fix this
    
    Auditors: jbn, atm, mib, jbr

--- src/test/java/dk/dbc/weekresolver/service/WeekResolverTest.java
+++ src/test/java/dk/dbc/weekresolver/service/WeekResolverTest.java
@@ -55,4 +55,50 @@ public class WeekResolverTest {
         assertThat(result.getCatalogueCode(), is("DPF"));
         assertThat(result.getWeekCode(), is("DPF202003"));
     }
+
+    @Test
+    public void TestDPFAtEaster2019() {
+
+        // This test uses the DPF cataloguecode to test the generic logic handling easter (and other closingdays).
+        // DPF should add 2 weeks and uses friday as shiftday
+        WeekResolver b = new WeekResolver()
+                .withCatalogueCode("DPF");
+
+        // take date
+        // add [0,1,2] weeks
+        // if closed => add 1 week
+        // if day-of-week = shiftday => add 1 week
+
+        // 3. apr. = wednesday => + 2 weeks = 17. apr., week 16, the day before maundy thursday.
+        // Not closed and not shiftday = 16
+        assertThat(b.withDate("2019-04-03").build().getWeekCode(), is("DPF201916"));
+
+        // 4. apr. = thursday => + 2 weeks = 18. apr., week 16, maundy thursday
+        // Closed, but not shiftday so add 1 week = week 17
+        assertThat(b.withDate("2019-04-04").build().getWeekCode(), is("DPF201917"));
+
+        // 5.apr. = friday => + 2 weeks = 19. apr., week 16, good friday
+        // Closed and shiftday so add 2 weeks = week 18
+        assertThat(b.withDate("2019-04-05").build().getWeekCode(), is("DPF201918"));
+
+        // 8. apr. = monday => + 2 weeks = 22. apr., week 17, easter monday
+        // Closed, not shiftday so add 1 week = 18
+        assertThat(b.withDate("2019-04-08").build().getWeekCode(), is("DPF201918"));
+
+        // 15. apr. = monday => + 2 weeks = 29. apr., week 18
+        // Not closed and not shiftday = 18
+        assertThat(b.withDate("2019-04-15").build().getWeekCode(), is("DPF201918"));
+
+        // 17. apr. = wednesday => + 2 weeks = 1. may., week 18
+        // Closed, not shiftday so add 1 week = 19
+        assertThat(b.withDate("2019-04-17").build().getWeekCode(), is("DPF201919"));
+
+        // 23. apr. = thursday => + 2 weeks = 7. may., week 19
+        // Not closed and not shiftday = 19
+        assertThat(b.withDate("2019-04-23").build().getWeekCode(), is("DPF201919"));
+
+        // 26. apr. = friday => + 2 weeks = 10. may., week 19
+        // Not closed but not shiftday = 20
+        assertThat(b.withDate("2019-04-23").build().getWeekCode(), is("DPF201920"));
+    }
 }
