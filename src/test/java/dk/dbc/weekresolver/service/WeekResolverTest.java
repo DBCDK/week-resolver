b2f4f61c1581bf9f41b47d93a5527fdbf79069d2
Henrik Witt-Hansen <hwha@dbc.dk>
2020-04-29 17:14:09 +0200

    Add tests for some new codes and the new locale setting
    Auditors: jbn, atm, jbr

--- src/test/java/dk/dbc/weekresolver/service/WeekResolverTest.java
+++ src/test/java/dk/dbc/weekresolver/service/WeekResolverTest.java
@@ -1,7 +1,6 @@
 package dk.dbc.weekresolver.service;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -13,6 +12,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.format.DateTimeParseException;
+import java.util.Locale;
 
 public class WeekResolverTest {
     final static String zone = "Europe/Copenhagen";
@@ -40,7 +40,7 @@ public class WeekResolverTest {
     }
 
     @Test
-    public void TestCatalogueCodeBPF() throws ParseException {
+    public void TestCatalogueCodeDPF() throws ParseException {
         WeekResolver b = new WeekResolver(zone)
                 .withCatalogueCode("dpf");
 
@@ -71,39 +71,39 @@ public class WeekResolverTest {
                 .withCatalogueCode("DPF");
 
         // take date
+        // if day-of-week >= shiftday then add days untill beginning of next week
         // add [0,1,2] weeks
-        // if closed => add 1 week
-        // if day-of-week = shiftday => add 1 week
+        // if closed => add days untill not a closing day
 
         // 3. apr. = wednesday => + 2 weeks = 17. apr., week 16, the day before maundy thursday.
         // Not closed and not shiftday = 16
         assertThat(b.withDate("2019-04-03").build().getWeekCode(), is("DPF201916"));
 
         // 4. apr. = thursday => + 2 weeks = 18. apr., week 16, maundy thursday
-        // Closed, but not shiftday so add 1 week = week 17
+        // Closed, but not shiftday so add 2 weeks and ajust for closing days = tuesday week 17
         assertThat(b.withDate("2019-04-04").build().getWeekCode(), is("DPF201917"));
 
-        // 5.apr. = friday => + 2 weeks = 19. apr., week 16, good friday
-        // Closed and shiftday so add 2 weeks = week 18
-        assertThat(b.withDate("2019-04-05").build().getWeekCode(), is("DPF201918"));
+        // 5.apr. = friday, shiftday => + 1 week + 2 weeks = 26. apr., week 17
+        // Shiftday so add 1 + 2 weeks = week 17
+        assertThat(b.withDate("2019-04-05").build().getWeekCode(), is("DPF201917"));
 
         // 8. apr. = monday => + 2 weeks = 22. apr., week 17, easter monday
-        // Closed, not shiftday so add 1 week = 18
-        assertThat(b.withDate("2019-04-08").build().getWeekCode(), is("DPF201918"));
+        // Closed, not shiftday so add 2 weeks and adjust for closing days = tuesday week 17
+        assertThat(b.withDate("2019-04-08").build().getWeekCode(), is("DPF201917"));
 
         // 15. apr. = monday => + 2 weeks = 29. apr., week 18
         // Not closed and not shiftday = 18
         assertThat(b.withDate("2019-04-15").build().getWeekCode(), is("DPF201918"));
 
         // 17. apr. = wednesday => + 2 weeks = 1. may., week 18
-        // Closed, not shiftday so add 1 week = 19
-        assertThat(b.withDate("2019-04-17").build().getWeekCode(), is("DPF201919"));
+        // Closed, not shiftday so add 2 weeks and adjust for closing days = 18
+        assertThat(b.withDate("2019-04-17").build().getWeekCode(), is("DPF201918"));
 
         // 23. apr. = thursday => + 2 weeks = 7. may., week 19
         // Not closed and not shiftday = 19
         assertThat(b.withDate("2019-04-23").build().getWeekCode(), is("DPF201919"));
 
-        // 26. apr. = friday => + 2 weeks = 10. may., week 19
+        // 26. apr. = friday, shiftday => + 1 + 2 weeks = 17. may., week 20
         // Not closed but shiftday = 20
         assertThat(b.withDate("2019-04-26").build().getWeekCode(), is("DPF201920"));
     }
@@ -143,7 +143,8 @@ public class WeekResolverTest {
         WeekResolver b = new WeekResolver(zone).withCatalogueCode("EMO");
         assertDoesNotThrow(() -> b.withDate("2019-11-29").build());
         assertThat(b.withDate("2019-11-23").build().getWeekCode(), is("EMO201948")); // saturday, week 47 = 48
-        assertThat(b.withDate("2019-11-24").build().getWeekCode(), is("EMO201949")); // sunday, week 47 = 49
+        assertThat(b.withDate("2019-11-24").build().getWeekCode(), is("EMO201948")); // sunday, week 47 = 48
+        assertThat(b.withDate("2019-11-25").build().getWeekCode(), is("EMO201949")); // monday, week 48 = 49
         assertThat(b.withDate("2019-12-26").build().getWeekCode(), is("EMO202001")); // wednesday, week 52 = 01
         assertThat(b.withDate("2020-04-09").build().getWeekCode(), is("EMO202016")); // thursday, week 15 = 16
         assertThat(b.withDate("2020-05-01").build().getWeekCode(), is("EMO202019")); // friday may 1.st (closing day ignored), week 18 = 19
@@ -160,9 +161,60 @@ public class WeekResolverTest {
         WeekResolver b = new WeekResolver(zone).withCatalogueCode("EMS");
         assertDoesNotThrow(() -> b.withDate("2019-11-29").build());
         assertThat(b.withDate("2019-11-23").build().getWeekCode(), is("EMS201948")); // saturday, week 47 = 48
-        assertThat(b.withDate("2019-11-24").build().getWeekCode(), is("EMS201949")); // sunday, week 47 = 49
+        assertThat(b.withDate("2019-11-24").build().getWeekCode(), is("EMS201948")); // sunday, week 47 = 48
+        assertThat(b.withDate("2019-11-25").build().getWeekCode(), is("EMS201949")); // monday, week 48 = 49
         assertThat(b.withDate("2019-12-26").build().getWeekCode(), is("EMS202001")); // wednesday, week 52 = 01
         assertThat(b.withDate("2020-04-09").build().getWeekCode(), is("EMS202016")); // thursday, week 15 = 16
         assertThat(b.withDate("2020-05-01").build().getWeekCode(), is("EMS202019")); // friday may 1.st (closing day ignored), week 18 = 19
+    }
+
+    @Test
+    public void TestCatalogueCodeACC() throws ParseException {
+
+        // ACC (and ACE, ACF, ACT, ACM, ARK, BLG):
+        //   - Current week number
+        //   - No shiftday
+        //   - No handling of closing days
+        //   - Allow end of year weeks
+        WeekResolver b = new WeekResolver(zone).withCatalogueCode("ACC");
+        assertDoesNotThrow(() -> b.withDate("2019-12-26").build());
+        assertThat(b.withDate("2019-12-26").build().getWeekCode(), is("ACC201952"));
+    }
+
+    @Test
+    public void TestFixedCodes() throws ParseException {
+
+        // Codes DBR, DBT, SDT should return a fixed code since they are used for retro updates
+        // and since we dont have the record data, we cannot set the correct weekcode
+        WeekResolver b = new WeekResolver();
+        assertDoesNotThrow(() -> b.withCatalogueCode("DBR").withDate("2019-12-26").build());
+        assertThat(b.withCatalogueCode("DBR").withDate("2019-12-26").build().getWeekCode(), is("DBR999999"));
+
+        assertDoesNotThrow(() -> b.withCatalogueCode("DBT").withDate("2019-12-26").build());
+        assertThat(b.withCatalogueCode("DBT").withDate("2019-12-26").build().getWeekCode(), is("DBT999999"));
+
+        assertDoesNotThrow(() -> b.withCatalogueCode("SDT").withDate("2019-12-26").build());
+        assertThat(b.withCatalogueCode("SDT").withDate("2019-12-26").build().getWeekCode(), is("SDT999999"));
+    }
+
+    @Test
+    public void TestLocaleParameter() {
+        WeekResolver b1 = new WeekResolver().withCatalogueCode("EMS");
+        WeekResolver b2 = new WeekResolver(zone, new Locale("da", "DK")).withCatalogueCode("EMS");
+        WeekResolver b3 = new WeekResolver().withCatalogueCode("EMS").withLocale(new Locale("da", "DK"));
+        WeekResolver b4 = new WeekResolver().withCatalogueCode("EMS").withLocale(new Locale("en", "US")); // Uses sunday as week begin
+
+        assertThat(b1.withDate("2019-11-24").build().getWeekCode(), is("EMS201948")); // sunday, week 47 = 48
+        assertThat(b1.withDate("2019-11-25").build().getWeekCode(), is("EMS201949")); // monday, week 48 = 49
+
+        assertThat(b2.withDate("2019-11-24").build().getWeekCode(), is("EMS201948")); // sunday, week 47 = 48
+        assertThat(b2.withDate("2019-11-25").build().getWeekCode(), is("EMS201949")); // monday, week 48 = 49
+
+        assertThat(b3.withDate("2019-11-24").build().getWeekCode(), is("EMS201948")); // sunday, week 47 = 48
+        assertThat(b4.withDate("2019-11-25").build().getWeekCode(), is("EMS201949")); // monday, week 48 = 49
+
+        assertThat(b4.withDate("2019-11-23").build().getWeekCode(), is("EMS201948")); // saturday, week 47 = 48
+        assertThat(b4.withDate("2019-11-24").build().getWeekCode(), is("EMS201949")); // sunday, week 47 (week 48 in us) = 49
+        assertThat(b4.withDate("2019-11-25").build().getWeekCode(), is("EMS201949")); // monday, week 48 = 49
     }
 }
